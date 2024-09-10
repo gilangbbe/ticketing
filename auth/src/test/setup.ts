b8d334be87549ce4dae3853ce707017238bb5571
gilangbbe <gilangbbe@gmail.com>
2024-09-10 11:54:38 +0700

    set up test for signin, signout, signup route handler

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,8 @@ import { app } from '../app';
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = 'natasha';
+
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
